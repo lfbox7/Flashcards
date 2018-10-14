6343acb613aa399459e60a8ffd3cb9eb49aa9d84
lfbox7 <lfbox7@yahoo.com>
2018-10-13 20:11:09 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Leonard Box on 10/13/18.
+//  Copyright © 2018 Leonard Box. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
